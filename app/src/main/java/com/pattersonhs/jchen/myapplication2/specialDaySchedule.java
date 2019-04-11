360c0516930f3c6d6e1b31c6d08b1b7b0bfdda89
JunlanChen <junlanchen0128@gmail.com>
2019-04-11 09:12:11 -0400

    update time

--- app/src/main/java/com/pattersonhs/jchen/myapplication2/specialDaySchedule.java
+++ app/src/main/java/com/pattersonhs/jchen/myapplication2/specialDaySchedule.java
@@ -20,6 +20,45 @@ public class specialDaySchedule extends AppCompatActivity {
         clipperWednesday = (Button) findViewById(R.id.clipperWednesday);
         twoHourDelay = (Button) findViewById(R.id.twoHourDelay);
 
-       
+        clipperWednesday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(AlarmClock.ACTION_SET_ALARM);
+                intent1.putExtra(AlarmClock.EXTRA_MESSAGE,"Start School Alarm");
+                intent1.putExtra(AlarmClock.EXTRA_HOUR,8);
+                intent1.putExtra(AlarmClock.EXTRA_MINUTES,40);
+                if (intent1 .resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent1);
+                }
+                Intent intent2 = new Intent(AlarmClock.ACTION_SET_ALARM);
+                intent2.putExtra(AlarmClock.EXTRA_MESSAGE,"2nd Period Start");
+                intent2.putExtra(AlarmClock.EXTRA_HOUR,9);
+                intent2.putExtra(AlarmClock.EXTRA_MINUTES,28);
+                if (intent2 .resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent2);
+                }
+                Intent intent3 = new Intent(AlarmClock.ACTION_SET_ALARM);
+                intent3.putExtra(AlarmClock.EXTRA_MESSAGE,"3rd Period Alarm");
+                intent3.putExtra(AlarmClock.EXTRA_HOUR,10);
+                intent3.putExtra(AlarmClock.EXTRA_MINUTES,21);
+                if (intent3 .resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent3);
+                }
+                Intent intent4 = new Intent(AlarmClock.ACTION_SET_ALARM);
+                intent4.putExtra(AlarmClock.EXTRA_MESSAGE,"4th Period Alarm");
+                intent4.putExtra(AlarmClock.EXTRA_HOUR,11);
+                intent4.putExtra(AlarmClock.EXTRA_MINUTES,04);
+                if (intent4 .resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent4);
+                }
+                Intent intent5 = new Intent(AlarmClock.ACTION_SET_ALARM);
+                intent5.putExtra(AlarmClock.EXTRA_MESSAGE,"5th Period Alarm");
+                intent5.putExtra(AlarmClock.EXTRA_HOUR,11);
+                intent5.putExtra(AlarmClock.EXTRA_MINUTES,47);
+                if (intent5 .resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent5);
+                }
+            }
+        });
     }
 }
